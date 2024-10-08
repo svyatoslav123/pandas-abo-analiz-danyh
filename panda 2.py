93f7a4fa8341390577647f8121190bbc412602c4
Lpopivnyak <136169348+LPopivnyak@users.noreply.github.com>
2024-10-08 17:28:46 +0300

    dad

--- panda 2.py
+++ panda 2.py
@@ -3,3 +3,4 @@ import pandas as pd
 
 df = pd.read_csv("GoogleApps (3).csv")
 print(df.groupby(by = 'Type')['Rating'].agg(['min','median',max]))
+temp = df.pivot_table(index = "Content Rating", columns = 'Category', )
